586d939258a38ea33c8204d337277b01bff35c30
bharath-designer <bharathv.developer@gmail.com>
2024-06-20 12:16:22 +0530

    Sorting added

---
+++ Day50_June_19/README.md
@@ -0,0 +1,5 @@
+## Quotes with pagination is live [here](https://bharath-designer.github.io/Genspark_Tasks/Day50_June_19) - Check it out
+
+
+### Preview:
+![](Output.png)
--- Day50_June_19/scripts/quotes.js
+++ Day50_June_19/scripts/quotes.js
@@ -1,6 +1,7 @@
 let data = []
 const QUOTES_PER_PAGE = 5;
 let currentIndex = 1
+let sortBy = '-'
 
 const fetchData = async () => {
     const rawData = await fetch(`https://dummyjson.com/quotes?limit=${QUOTES_PER_PAGE}&skip=${QUOTES_PER_PAGE * (currentIndex - 1)}`)
@@ -10,12 +11,11 @@ const fetchData = async () => {
 const manipulateContainer = async () => {
     const quotesContainer = document.querySelector(".quotes")
     quotesContainer.innerHTML = "Loading quotes..."
-
     quotesContainer.innerHTML = ""
     data.quotes.forEach(quote => {
         const template = document.createElement("template")
-        template.innerHTML = 
-        `
+        template.innerHTML =
+            `
             <div class="quote">
                     <span class="value">
                        ${quote.quote}
@@ -30,20 +30,37 @@ const manipulateContainer = async () => {
 }
 
 fetchData()
-.then(()=>{
-    manipulateContainer()
-})
+    .then(() => {
+        manipulateContainer()
+    })
 
 const changePagination = async (option) => {
-    clearInp()
     if (option === 'next') {
         currentIndex++
-    } else {
+    } else if (option == 'prev') {
         if (currentIndex == 1) return;
         currentIndex--
     }
-    await fetchData()
-    await manipulateContainer()
+    
+    if (sortBy == '-') {
+        clearInp()
+        await fetchData()
+        await manipulateContainer()
+    } else {
+        document.querySelector(".search-inp").value = ""
+        const rawData = await fetch(`https://dummyjson.com/quotes`)
+        data = await rawData.json()
+        if (sortBy == 'asc') {
+            data.quotes.sort((a, b) => a.author.localeCompare(b.author));
+        } else {
+            data.quotes.sort((a, b) => b.author.localeCompare(a.author));
+        }
+        const startIndex = (currentIndex - 1) * QUOTES_PER_PAGE;
+        const endIndex = startIndex + QUOTES_PER_PAGE;
+        data.quotes.splice(endIndex);
+        data.quotes.splice(0, startIndex);
+        await manipulateContainer()
+    }
 }
 
 
@@ -58,7 +75,27 @@ const search = async () => {
 
     const rawData = await fetch(`https://dummyjson.com/quotes`)
     const parsedData = await rawData.json()
-    data = {quotes: parsedData.quotes.filter(element => element.quote.toLowerCase().includes(searchText.toLowerCase()) || element.author.toLowerCase().includes(searchText.toLowerCase()))}
+    data = { quotes: parsedData.quotes.filter(element => element.quote.toLowerCase().includes(searchText.toLowerCase()) || element.author.toLowerCase().includes(searchText.toLowerCase())) }
     manipulateContainer();
 }
 
+
+
+const sort = async (value) => {
+    const btn = document.querySelector(".sort-btn > button")
+    sortBy = value
+    if (sortBy == '-') {
+        btn.textContent = "Sort"
+        currentIndex = 1
+        await fetchData()
+        await manipulateContainer()
+        return;
+    }
+    if (sortBy == 'asc') {
+        btn.textContent = "A-Z"
+    } else {
+        btn.textContent = "Z-A"
+    }
+    currentIndex = 1
+    changePagination()
+}
